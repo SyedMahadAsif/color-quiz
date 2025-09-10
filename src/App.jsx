db2c149aae0cd588040c10a114222ffc3b7922a8
SyedMahadAsif <mahadasif89@gmail.com>
2025-09-11 01:33:59 +0500

    added more width on pc

--- src/App.jsx
+++ src/App.jsx
@@ -189,7 +189,7 @@
 
     return (
       <div className="min-h-screen bg-black flex items-center justify-center px-4 py-10">
-      <div className="w-full max-w-md sm:max-w-2xl bg-white rounded-2xl shadow-xl p-6 sm:p-8 transition-all duration-300">
+      <div className="w-full max-w-md sm:max-w-4xl bg-white rounded-2xl shadow-xl p-6 sm:p-8 transition-all duration-300">
 
         {/* Header */}
         <header className="mb-6 flex justify-between items-start">
